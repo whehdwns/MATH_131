d34f376b62b11ddfc8586143348e8f34b2a43368
DongJun Cho (DJ) <dongjun819@gmail.com>
2019-11-22 16:19:17 -0800

    Update Exercise0.m

--- assignment0/Exercise0.m
+++ assignment0/Exercise0.m
@@ -1,37 +1,12 @@
+x1 = mysum(20)
+x2 = mysum(100)
 
-x1 = myalternatesum1(20)
-x2 = myalternatesum1(100)
-x3 = myderivative(1.1)
-function result = myalternatesum1(n)
-    result =0;
-    for i = 1: n
-        result = result + i*((-1)^i);
+function Exercise1 = mysum(n)
+    b = 0; %start from 0
+    for i = 1:n  %adding the numbers up to n
+        b = b + i; % adding number / number increment by 1
     end
- %   fprintf('%f', result);
+    Exercise1 = b; %when it reaches to n, it returns the result value.
 end
-%pseudocode
-%input x
-%output y
-%function myalternatesum
-%   for i is equal to 1 and go up to n
-%       result = result + i*((-1)^i)) from formula
-%end 
-
-%f= @(x) 0.5*(x-1).^2;
-%format long
-%format short
-%f(sqrt(3))
-
-%f= @(x) (x-1).^2;
-%h =@(x,h)(f(x+h)-f(x))/h;
-%h(1.1, 0.01)
-%h(1.1,0.001)
-
-
-function result = myderivative(x)
-    result= 0.5*(x-1).^2;
-end
-%f= @(x) 0.5*(x-1).^2;
-%h =@(x,h)(f(x+h)-f(x))/h;
-%h(1.1, 0.1)
-%h(1.1, 0.01)
+%This mysum function is adding the number from 0 to n value.
+%For example, 1+2+3+4+5+6+......+n
